2f0e0d9ddb52f97dd43203a6c0d249c508d161df
ranaacy174S <sunilrana1730@gmail.com>
2022-08-17 10:53:47 +0530

    updated one

--- message.js
+++ message.js
@@ -3,16 +3,17 @@ const express=require('express')
 const msgs=require('./data.js');
 
 const router=express.Router();
-
+// we  create   username  as  hidden button and pulled out  data  from localstorage;
 router.get('/',(req,res,next)=>{
     res.send('<form onsubmit="document.getElementById(`username`).value=localStorage.getItem(`user`) " action="/message" method="POST"> <input type="text" name="message" id="message" placeholder="Type your Message Here"> <input type="hidden" id="username" name="username" > <button type="submit">Submit</button></form>')
 })
 router.post('/message',(req,res,next)=>{
-    const message=req.body.message;
-    const obj=`${req.body.username}:${message}`
-    msgs.push(obj);
-    console.log(msgs)
-    res.redirect('/');
+   const message = req.body.message;
+    const username=  req.body.username;
+    const stringli  = `${username +":"+message}`
+
+   console.log(stringli);
+    res.redirect('/login');
 })
 
 module.exports=router;
